b9f5bec42b4adbc5633fa26715d079394f2e5a1b
Josh Hackney <josh.hackney13@gmail.com>
2020-10-27 18:26:09 -0400

    chapter 8 excercises all complete

--- 08-numbers-and-strings/02-strings/exercises/03-raw-concat/main.go
+++ 08-numbers-and-strings/02-strings/exercises/03-raw-concat/main.go
@@ -8,7 +8,10 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 // ---------------------------------------------------------
 // EXERCISE: Raw Concat
@@ -39,7 +42,11 @@ func main() {
 	// replace and concatenate the `name` variable
 	// after `hi ` below
 
-	msg := `hi CONCATENATE-NAME-VARIABLE-HERE!
+	name := os.Args[1]
+
+	// 	msg := `hi CONCATENATE-NAME-VARIABLE-HERE!
+	// how are you?`
+	msg := `hi ` + name + `!
 how are you?`
 
 	fmt.Println(msg)
--- 08-numbers-and-strings/02-strings/exercises/06-tolowercase/main.go
+++ 08-numbers-and-strings/02-strings/exercises/06-tolowercase/main.go
@@ -8,6 +8,12 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: ToLowercase
 //
@@ -28,4 +34,6 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+	name := strings.ToLower(os.Args[1])
+	fmt.Println(name)
 }
