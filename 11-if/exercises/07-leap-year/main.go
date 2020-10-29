d9c8339c8bebe3c5d22625ccb49e10ce8a2781fb
Josh Hackney <josh.hackney13@gmail.com>
2020-10-29 13:53:25 -0400

    exercise 07 ch 11 completed

--- 11-if/exercises/07-leap-year/main.go
+++ 11-if/exercises/07-leap-year/main.go
@@ -8,6 +8,12 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Leap Year
 //
@@ -37,4 +43,29 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Give me a year number")
+		return
+	}
+
+	year, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("%q is not a valid year\n", os.Args[1])
+		return
+	}
+
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 == 0 {
+				fmt.Printf("%s is a leap year\n", os.Args[1])
+			} else {
+				fmt.Printf("%s is not a leap year\n", os.Args[1])
+				return
+			}
+		}
+		fmt.Printf("%s is a leap year\n", os.Args[1])
+	} else {
+		fmt.Printf("%s is not a leap year\n", os.Args[1])
+	}
+
 }
