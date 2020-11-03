111f6bea227c1eb3a76354035bd988464eda4e30
Josh Hackney <josh.hackney13@gmail.com>
2020-11-03 13:31:19 -0500

    ch 13 exercise 04 completed

--- 13-loops/exercises/04-only-evens/main.go
+++ 13-loops/exercises/04-only-evens/main.go
@@ -8,6 +8,12 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Only Evens
 //
@@ -28,4 +34,28 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("gimme two numbers")
+		return
+	}
+
+	min, err1 := strconv.Atoi(os.Args[1])
+	max, err2 := strconv.Atoi(os.Args[2])
+	if err1 != nil || err2 != nil || min >= max {
+		fmt.Println("wrong numbers")
+		return
+	}
+
+	var sum int
+	for i := min; i <= max; i++ {
+		if i%2 == 0 {
+			sum += i
+			fmt.Print(i)
+			if i < max {
+				fmt.Print(" + ")
+			}
+		}
+
+	}
+	fmt.Printf(" = %d\n", sum)
 }
